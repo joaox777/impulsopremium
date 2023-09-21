edaaf34b87b724482ce4f14226c38cc53fb531c2
João Victor Ornelas <84112302+joaox777@users.noreply.github.com>
2023-09-20 22:26:32 -0300

    add menu

---
+++ scriptmenu.js
@@ -0,0 +1,12 @@
+// Função para mostrar/ocultar o menu em telas menores
+function toggleMenu() {
+    var menu = document.getElementById('header').querySelector('ul');
+    if (menu.style.display === 'block' || menu.style.display === '') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
+
+// Adicione um evento de clique ao botão do menu de hambúrguer
+document.getElementById('menu-button').addEventListener('click', toggleMenu);

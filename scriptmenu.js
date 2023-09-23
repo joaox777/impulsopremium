0d5a0afafa8c9a031a62e715ffe0ad02aaad36a0
João Victor Ornelas <84112302+joaox777@users.noreply.github.com>
2023-09-23 15:19:10 -0300

    slider -menu modification

--- scriptmenu.js
+++ scriptmenu.js
@@ -1,17 +1,31 @@
 
-
-// Função para mostrar/ocultar o menu em telas menores
+// Função para mostrar/ocultar o menu em telas menores e ajustar a posição do botão
 function toggleMenu() {
-    var menu = document.getElementById('header').querySelector('ul');
-    if (menu.style.display === 'block' || menu.style.display === '') {
-        menu.style.display = 'none';
+    var menu = document.getElementById('menu');
+    var menuButton = document.getElementById('menu-button');
+    
+    if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
+        menuButton.classList.remove('active');
     } else {
-        menu.style.display = 'block';
+        menu.classList.add('active');
+        menuButton.classList.add('active');
     }
 }
 
-// Adicione um evento de clique ao botão do menu de hambúrguer
-document.getElementById('menu-button').addEventListener('click', toggleMenu);
+
+// Adicione um ouvinte de eventos para o botão de hambúrguer
+var menuButton = document.getElementById('menu-button');
+menuButton.addEventListener('click', toggleMenu);
+
+// Adicione um ouvinte de eventos para ocultar o menu quando um link for clicado
+var menuLinks = document.querySelectorAll('#menu a');
+menuLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+        var menu = document.getElementById('menu');
+        menu.classList.remove('active');
+    });
+});
 
 
 // script slider
